8312175b6c1140df5cf8468f15e75f3f50548202
Aman <Amandeep2ra@gmail.com>
2022-07-01 00:42:53 -0400

    Images in index should finally be loading

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,3 @@
-var loc = window.location.pathname;
-var dir = loc.substring(0, loc.lastIndexOf("/"));
-console.log(loc);
-console.log(dir);
-
 const projectsToggle = document.getElementById("projects-slider-toggle");
 
 projectsToggle.addEventListener("click", function () {
@@ -10,90 +5,52 @@ projectsToggle.addEventListener("click", function () {
 });
 
 const carouselTrack = document.querySelector(".carousel-track");
-let imgDir = "../assets/images/General/";
+const user = "Dino-Jesus";
+const repo = "Dino-Jesus.github.io";
+const imgDir = "assets/images/Highlighted Projects/";
+const imgUrl =
+  "https://api.github.com/repos/" + user + "/" + repo + "/contents/" + imgDir;
 $(document).ready(function () {
-  document.querySelector(".imtwo").src = "../assets/images/General/Logo.png";
-  document.querySelector(".imone").src =
-    "../assets/images/Highlighted Projects/Slide1.png";
   $.ajax({
-    url: imgDir,
+    url: imgUrl,
     success: function (data) {
       console.log(data);
-      $(data)
-        .find("a")
-        .attr("href", function (i, val) {
-          if (val.match(/\.(jpe?g|png|gif)$/)) {
-            let start, end;
-            for (let i = val.length - 1; i >= 0; i--) {
-              if (!end && val[i] == ".") {
-                end = i;
-              } else if (!start && val[i] == "/") {
-                start = i + 1;
-              } else if (start && end) {
-                break;
-              }
+      for (let file of data) {
+        console.log(
+          `file path: ${file.path} \nfile name: ${
+            file.name
+          } \nfile is img? ${file.name.match(
+            /\.(jpe?g|png|gif)$/
+          )} \nfile dw url: ${file.download_url}`
+        );
+        let val = file.name;
+        if (val.match(/\.(jpe?g|png|gif)$/)) {
+          let start, end;
+          for (let i = val.length - 1; i >= 0; i--) {
+            if (!end && val[i] == ".") {
+              end = i;
+            } else if (!start && val[i] == "/") {
+              start = i + 1;
+            } else if (!start && i == 0) {
+              start = i;
+            } else if (start && end) {
+              break;
             }
-            $(".names-track").append(
-              '<li class="slide-name"><h3>' +
-                val.slice(start, end).replace("%20", " ") +
-                "</h3></li>"
-            );
-            $(".carousel-track").append(
-              ' <li class="carousel-slide current-slide"><img class="carousel-image" src="' +
-                val +
-                '" alt=""/></li>'
-            );
-            $(".carousel-nav").append(
-              '<button class="carousel-indicator"></button>'
-            );
           }
-        });
+          $(".names-track").append(
+            '<li class="slide-name"><h3>' + val.slice(start, end) + "</h3></li>"
+          );
+          $(".carousel-track").append(
+            ' <li class="carousel-slide current-slide"><img class="carousel-image" src="' +
+              file.download_url +
+              '" alt=""/></li>'
+          );
+          $(".carousel-nav").append(
+            '<button class="carousel-indicator"></button>'
+          );
+        }
+      }
       refreshCarousel(document.querySelector(".projects-carousel"));
-    },
-    error: function (XMLHttpRequest, textStatus, errorThrown) {
-      alert("Status: " + textStatus);
-      alert("Error: " + errorThrown);
-    },
-  });
-  imgDir = "../assets/images/textures/";
-  $.ajax({
-    url: imgDir,
-    success: function (data) {
-      console.log(data);
-      $(data)
-        .find("a")
-        .attr("href", function (i, val) {
-          if (val.match(/\.(jpe?g|png|gif)$/)) {
-            let start, end;
-            for (let i = val.length - 1; i >= 0; i--) {
-              if (!end && val[i] == ".") {
-                end = i;
-              } else if (!start && val[i] == "/") {
-                start = i + 1;
-              } else if (start && end) {
-                break;
-              }
-            }
-            $(".names-track").append(
-              '<li class="slide-name"><h3>' +
-                val.slice(start, end).replace("%20", " ") +
-                "</h3></li>"
-            );
-            $(".carousel-track").append(
-              ' <li class="carousel-slide current-slide"><img class="carousel-image" src="' +
-                val +
-                '" alt=""/></li>'
-            );
-            $(".carousel-nav").append(
-              '<button class="carousel-indicator"></button>'
-            );
-          }
-        });
-      refreshCarousel(document.querySelector(".projects-carousel"));
-    },
-    error: function (XMLHttpRequest, textStatus, errorThrown) {
-      alert("Status: " + textStatus);
-      alert("Error: " + errorThrown);
     },
   });
 });
