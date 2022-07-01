35dc201c4d382430b68e30290ca3ea5c76e0004b
Aman <Amandeep2ra@gmail.com>
2022-06-30 20:53:39 -0400

    test 3?

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,10 +14,48 @@ let imgDir = "../assets/images/General/";
 $(document).ready(function () {
   document.querySelector(".imtwo").src = "../assets/images/General/Logo.png";
   document.querySelector(".imone").src =
-    "../assets/images/Highlighted Projects/Slide1.PNG";
+    "../assets/images/Highlighted Projects/Slide1.png";
   $.ajax({
     url: imgDir,
     success: function (data) {
+      console.log(data);
+      $(data)
+        .find("a")
+        .attr("href", function (i, val) {
+          if (val.match(/\.(jpe?g|png|gif)$/)) {
+            let start, end;
+            for (let i = val.length - 1; i >= 0; i--) {
+              if (!end && val[i] == ".") {
+                end = i;
+              } else if (!start && val[i] == "/") {
+                start = i + 1;
+              } else if (start && end) {
+                break;
+              }
+            }
+            $(".names-track").append(
+              '<li class="slide-name"><h3>' +
+                val.slice(start, end).replace("%20", " ") +
+                "</h3></li>"
+            );
+            $(".carousel-track").append(
+              ' <li class="carousel-slide current-slide"><img class="carousel-image" src="' +
+                val +
+                '" alt=""/></li>'
+            );
+            $(".carousel-nav").append(
+              '<button class="carousel-indicator"></button>'
+            );
+          }
+        });
+      refreshCarousel(document.querySelector(".projects-carousel"));
+    },
+  });
+  let imgDir = "../assets/images/General";
+  $.ajax({
+    url: imgDir,
+    success: function (data) {
+      console.log(data);
       $(data)
         .find("a")
         .attr("href", function (i, val) {
